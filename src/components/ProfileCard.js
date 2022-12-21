0ed5d1488158297b925ad7d4cf0fa844ff4bee55
lawyerh <lawyerh_01@yahoo.com>
2022-12-21 15:45:17 -0800

    refactored profilecard component with javascript destructuring

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,13 +1,12 @@
-function ProfileCard(props) {
-  console.log(props.img);
+function ProfileCard({ profileName, profileHandle, profileDescription, img }) {
   return (
     <div className="card">
       <div className="card__photo-container">
-        <img src={props.img} alt="pda cover" className="card__photo" />
+        <img src={img} alt="pda cover" className="card__photo" />
       </div>
-      <p className="card__name">{props.profileName}</p>
-      <p className="card__handle">{props.profileHandle}</p>
-      <p className="card__description">{props.profileDescription}</p>
+      <p className="card__name">{profileName}</p>
+      <p className="card__handle">{profileHandle}</p>
+      <p className="card__description">{profileDescription}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+
 // Get reference to the div with id Root
 const el = document.getElementById("root");
+
 // Tell react to take control of that element
 const root = ReactDOM.createRoot(el);
 
