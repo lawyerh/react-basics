64a36d68336ffa3860f053217dd06aebb9af48ef
lawyerh <lawyerh_01@yahoo.com>
2022-12-21 16:34:00 -0800

    changed profilecard style and added more consistent sizes for photos

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,9 +4,11 @@ function ProfileCard({ profileName, profileHandle, profileDescription, img }) {
       <div className="card__photo-container">
         <img src={img} alt="pda cover" className="card__photo" />
       </div>
-      <p className="card__name">{profileName}</p>
-      <p className="card__handle">{profileHandle}</p>
-      <p className="card__description">{profileDescription}</p>
+      <div className="card__text-container">
+        <p className="card__name">{profileName}</p>
+        <p className="card__handle">{profileHandle}</p>
+        <p className="card__description">{profileDescription}</p>
+      </div>
     </div>
   );
 }
